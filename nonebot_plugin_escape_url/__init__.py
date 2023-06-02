3c3b3bfa7e5f3dcfb0227182d3cad3c6eba75643
ssttkkl <huang.wen.long@hotmail.com>
2023-06-02 11:07:44 +0800

    添加插件元数据

--- nonebot_plugin_escape_url/__init__.py
+++ nonebot_plugin_escape_url/__init__.py
@@ -1,2 +1,10 @@
+from nonebot.plugin import PluginMetadata
+
+__plugin_meta__ = PluginMetadata(
+    name="链接防夹",
+    description="自动转换发送消息中的URL，逃避URL检测",
+    usage="无"
+)
+
 from . import onebot_v11
 from . import qqguild
