b8eecc90654bc219ec2b7326c18b3f7c9620eb68
ssttkkl <huang.wen.long@hotmail.com>
2023-06-02 14:48:23 +0800

    更新插件元数据

--- nonebot_plugin_escape_url/__init__.py
+++ nonebot_plugin_escape_url/__init__.py
@@ -1,9 +1,15 @@
 from nonebot.plugin import PluginMetadata
 
+from .config import Config
+
 __plugin_meta__ = PluginMetadata(
     name="链接防夹",
     description="自动转换发送消息中的URL，逃避URL检测",
-    usage="无"
+    usage="无",
+    type="application",
+    homepage="https://github.com/bot-ssttkkl/nonebot-plugin-escape-url",
+    config=Config,
+    supported_adapters={"~onebot.v11", "~qqguild"}
 )
 
 from . import onebot_v11
